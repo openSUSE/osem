1bd779b6c1307aaac8a6146075a5dbdada445b73
differentreality <differentreality@gmail.com>
2013-07-11 07:47:05 +0300

    fix registration model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -5,6 +5,7 @@ class Registration < ActiveRecord::Base
 
   has_one :supporter_registration
   has_and_belongs_to_many :social_events
+  has_and_belongs_to_many :events
 
   attr_accessible :person_id, :conference_id, :attending_social_events, :attending_with_partner,
                   :using_affiliated_lodging, :arrival, :departure, :person_attributes, :other_dietary_choice, :dietary_choice_id,
