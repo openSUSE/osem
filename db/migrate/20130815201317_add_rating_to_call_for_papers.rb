e6b7fbceb508ba56be2c47f07e637a5637b4d4f7
differentreality <differentreality@gmail.com>
2013-09-02 16:38:43 +0300

    fix

--- db/migrate/20130815201317_add_rating_to_call_for_papers.rb
+++ db/migrate/20130815201317_add_rating_to_call_for_papers.rb
@@ -1,5 +1,5 @@
 class AddRatingToCallForPapers < ActiveRecord::Migration
   def change
-    add_column :call_for_papers, :rating, :integer
+    add_column :call_for_papers, :rating, :integer, :null => 1
   end
 end
