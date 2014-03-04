5b05ffbd564a25c9593e8066524458a1e46ff70e
Henne Vogelsang <hvogel@opensuse.org>
2014-03-04 10:30:23 +0100

    Rename the volunteers to match the other config routes

--- app/controllers/admin/volunteers_controller.rb
+++ app/controllers/admin/volunteers_controller.rb
@@ -11,13 +11,11 @@ class Admin::VolunteersController < ApplicationController
 
   def update
     @conference = Conference.find_all_by_short_title(params[:conference_id]).first
-#     @conference.update_attributes(params[:conference])
-#     redirect_to admin_conference_volunteers_path(@conference.short_title)
-        begin
+    begin
       @conference.update_attributes!(params[:conference])
-       redirect_to(admin_conference_volunteer_options_path(:conference_id => params[:conference_id]), :notice => "Volunteering options were successfully updated.")
-        rescue Exception => e
-          redirect_to(admin_conference_volunteer_options_path(:conference_id => params[:conference_id]), :alert => "Volunteering options update failed: #{e.message}")
+      redirect_to(admin_conference_volunteer_info_path(:conference_id => params[:conference_id]), :notice => "Volunteering options were successfully updated.")
+    rescue Exception => e
+      redirect_to(admin_conference_volunteer_info_path(:conference_id => params[:conference_id]), :alert => "Volunteering options update failed: #{e.message}")
     end
   end
 end
