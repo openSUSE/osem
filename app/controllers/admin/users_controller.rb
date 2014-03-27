481fcc9f60859a283935ab77e241e7bc72128730
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-03-27 20:14:52 +0530

    Admin able to delete users except admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,5 +15,17 @@ class Admin::UsersController < ApplicationController
     user = User.find(params[:id])
     user.update_attributes!(params[:user])
     redirect_to admin_users_path, :notice => "Updated #{user.email}"
+  end
+
+  def delete
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, :notice => "User got deleted"
+    
   end  
+
 end
