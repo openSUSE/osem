33d74277d0165e5d55c074cfee67c05b47558c79
Henne Vogelsang <hvogel@opensuse.org>
2014-04-10 11:59:24 +0200

    Make the travis log shorter, enable coveralls rails mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+require 'coveralls'
+Coveralls.wear!('rails')
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'coveralls'
-Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
