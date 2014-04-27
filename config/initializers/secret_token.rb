019b570a70a147011af0dd4b93d3a7144d78993b
KalabiYau <skullzeek@gmail.com>
2014-04-27 18:03:47 +0200

    Remove insecure storage of cookies secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Osem::Application.config.secret_token = '49fe86302d3297fdccac1614834f3036841416b22d0ea0b04ae140ce512e1d736f4ef796b85097cce902760071838b10d1ac0e3cf02f55fd2d1031d4fce2f956'
+Osem::Application.config.secret_token = CONFIG['cookies_secret_token']
