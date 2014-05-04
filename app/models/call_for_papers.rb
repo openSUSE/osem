e3d21de1cb4e01d20307684476a359f312eecdc1
Stella <differentreality@gmail.com>
2014-05-04 13:09:49 +0300

    Fix cfp create and error display

--- app/models/call_for_papers.rb
+++ app/models/call_for_papers.rb
@@ -2,7 +2,7 @@ class CallForPapers < ActiveRecord::Base
   attr_accessible :start_date, :end_date, :hard_deadline, :description, :schedule_changes, :rating, :rating_desc, :schedule_public
   belongs_to :conference
 
-  validates_presence_of :start_date, :end_date, :hard_deadline
+  validates_presence_of :start_date, :end_date, :hard_deadline, :description
   validates :rating, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }
 
 end
