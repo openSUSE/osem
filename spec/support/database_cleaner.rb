f96ff6ed17f67fa1c95fca47a493dce39a4136ba
Chrisbr <cbruckmayer@gmx.net>
2014-05-10 16:02:44 +0200

    Fix Hound CI violations

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -8,7 +8,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, :js => true) do
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
 
