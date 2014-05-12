d50fd3cc23f1b6010f3edd967a6795c67c9c9fd3
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-05-12 17:57:35 +0530

    Fixed houndci violations

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,26 +1,21 @@
 require 'spec_helper'
 
 describe Admin::UsersController do
-
   before(:each) do
     @user = create(:user)
     @admin = create(:admin)
     sign_in(@admin)
   end
-
   describe "GET '#index'" do
-    
     it 'populates an array of users' do
-      user1 = create(:user, email:"gopesh.7500@gmail.com")
-      user2 = create(:user,email:"gopesh@gmail.com")
+      user1 = create(:user, email: "gopesh.7500@gmail.com")
+      user2 = create(:user, email: "gopesh@gmail.com")
       get :index
-      # pp response.body
       expect(assigns(:users)).to match_array([@user,@admin,user1,user2])
     end
 
     it 'renders index template' do
       get :index
-      # pp response.body
       expect(response).to render_template :index
     end
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@
 
 FactoryGirl.define do
   factory :user do
-    sequence(:email) {|n|'name#{n}@example.com'}
+    sequence(:email) { |n| 'name#{n}@example.com' }
     password 'changeme'
     password_confirmation 'changeme'
     confirmed_at Time.now
   end
   factory :admin, class: User do
-   	sequence(:email) {|n|'gopesh#{n}@exampleco.in'}
+   	sequence(:email) { |n| 'gopesh#{n}@exampleco.in' }
     password 'changeme'
     password_confirmation 'changeme'
     confirmed_at Time.now
