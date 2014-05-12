27f35a49c630c39c74d9b26dfcfd4af90bd92ec4
Chrisbr <cbruckmayer@gmx.net>
2014-05-12 14:38:55 +0200

    Fix create conference form - adds HTML 5 validations

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -73,7 +73,7 @@ Formtastic::FormBuilder.required_string = "(required)"
 # You can opt-in to new HTML5 browser validations (for things like email and url inputs) by setting
 # this to false. Doing so will add a `novalidate` attribute to the `<form>` tag.
 # See http://diveintohtml5.org/forms.html#validation for more info.
-# Formtastic::FormBuilder.perform_browser_validations = true
+Formtastic::FormBuilder.perform_browser_validations = true
 #
 Formtastic::Helpers::FormHelper.builder = FormtasticBootstrap::FormBuilder
 FormtasticBootstrap::FormBuilder.default_inline_hint_class = FormtasticBootstrap::FormBuilder.default_block_hint_class
