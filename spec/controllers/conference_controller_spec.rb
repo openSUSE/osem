116b862e98262b41e8bf23b7f33d47b0caa39c15
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-05-12 20:45:53 +0530

    fixes by houndci

--- spec/controllers/conference_controller_spec.rb
+++ spec/controllers/conference_controller_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe ConferenceController do
 
-  describe "GET 'show'" do
-    it "returns http success" do
+  describe 'GET #show' do
+    it 'returns http success' do
       get :show, :id => 'sample'
       expect(response).to be_success
     end
