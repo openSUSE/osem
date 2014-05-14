17307736e8aba937db542db616bd56ab5b9b09f6
Chrisbr <cbruckmayer@gmx.net>
2014-05-14 16:37:19 +0200

    Removes rating desc column from cfp

---
+++ db/migrate/20140514140013_remove_rating_desc_from_cfp.rb
@@ -0,0 +1,9 @@
+class RemoveRatingDescFromCfp < ActiveRecord::Migration
+  def up
+    remove_column :call_for_papers, :rating_desc
+  end
+
+  def down
+    add_column :call_for_papers, :rating_desc, :text
+  end
+end
