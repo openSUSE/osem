85cc4413bda7ed571ce7355849b93941991b8686
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-05-16 12:46:50 +0530

    fixes by houndci

--- app/models/event_attachment.rb
+++ app/models/event_attachment.rb
@@ -4,8 +4,8 @@ class EventAttachment < ActiveRecord::Base
   attr_accessible :public, :attachment, :event_id, :title
   has_attached_file :attachment
   validates_attachment_content_type :attachment, content_type: 
-    ['text/plain', 'image/jpg', 'image/jpeg','image/pjpeg', 'image/png', 'image/x-png', 'image/gif',
-      'application/pdf', 'application/msword','applicationvnd.ms-word', 
+    ['text/plain', 'image/jpg', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/x-png', 'image/gif',
+      'application/pdf', 'application/msword', 'applicationvnd.ms-word', 
       'applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document',
       'application/msexcel', 'application/vnd.ms-excel',
       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -14,7 +14,6 @@ class EventAttachment < ActiveRecord::Base
       'application/vnd.oasis.opendocument.presentation',
       'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
   include Rails.application.routes.url_helpers
-
   def to_jq_upload
     {
       'name' => read_attribute(:attachment_file_name),
@@ -33,6 +32,4 @@ class EventAttachment < ActiveRecord::Base
   #    :url => "/system/:attachment/:id/:style/:filename"
 
   #has_paper_trail :meta => {:associated_id => :event_id, :associated_type => "Event"}
-
-
 end
