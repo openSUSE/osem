8c69d0f6511f1b14141aeba48cfef4fe2456c254
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-05-16 12:14:08 +0530

    fixes by houndci

--- app/models/event_attachment.rb
+++ app/models/event_attachment.rb
@@ -1,16 +1,17 @@
 class EventAttachment < ActiveRecord::Base
   has_paper_trail
-
   belongs_to :event
   attr_accessible :public, :attachment, :event_id, :title
-  
   has_attached_file :attachment
-  validates_attachment_content_type :attachment, :content_type => ['text/plain','image/jpg','image/jpeg',
-    'image/pjpeg','image/png','image/x-png','image/gif','application/pdf','application/msword','applicationvnd.ms-word',
-    'applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document','application/msexcel',
-    'application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/mspowerpoint',
-    'application/vnd.ms-powerpoint','application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    'application/vnd.oasis.opendocument.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+  validates_attachment_content_type :attachment, content_type: ['text/plain','image/jpg','image/jpeg',
+    'image/pjpeg','image/png','image/x-png','image/gif','application/pdf','application/msword',
+    'applicationvnd.ms-word','applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document',
+    'application/msexcel','application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/mspowerpoint','application/vnd.ms-powerpoint',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'application/vnd.oasis.opendocument.presentation',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
   include Rails.application.routes.url_helpers
 
   def to_jq_upload
