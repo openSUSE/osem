0257c73176be4b1d7346afdba2fb23f97ed3a590
Stella <differentreality@gmail.com>
2014-05-19 13:17:32 +0300

    translation, first approach

--- config/routes.rb
+++ config/routes.rb
@@ -1,83 +1,66 @@
 Osem::Application.routes.draw do
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 
-  devise_for :users, :controllers => { :registrations => :registrations }, :path => 'accounts'
+    devise_for :users, :controllers => { :registrations => :registrations }, :path => 'accounts'
 
-  namespace :admin do
-    resources :users
-    resources :people
-    resources :conference do
-      get "/schedule" => "schedule#show"
-      patch "/schedule" => "schedule#update"
-      get "/stats" => "stats#index"
-      get "/registrations" => "registrations#show"
-      get "/registrations/new" => "registrations#new"
-      patch "/registrations/new" => "registrations#create"
-      get "/registrations/edit" => "registrations#edit"
-      patch "/registrations/edit" => "registrations#update"
-      delete "/registrations"  => "registrations#delete"
-      patch "/registrations/change_field" => "registrations#change_field"
-      get "/venue" => "venue#show", :as => "venue_info"
-      patch "/venue" => "venue#update", :as => "venue_update"
-      get "/dietary_choices" => "dietchoices#show", :as => "dietary_list"
-      patch "/dietary_choices" => "dietchoices#update", :as => "dietary_update"
-      get "/volunteers_list" => "volunteers#show"
-      get "/volunteers" => "volunteers#index", :as => "volunteers_info"
-      patch "/volunteers" => "volunteers#update", :as => "volunteers_update"
+    namespace :admin do
+      resources :users
+      resources :people
+      resources :conference do
+        get "/schedule" => "schedule#show"
+        put "/schedule" => "schedule#update"
+        get "/stats" => "stats#index"
+        get "/registrations" => "registrations#show"
+        get "/registrations/new" => "registrations#new"
+        put "/registrations/new" => "registrations#create"
+        get "/registrations/edit" => "registrations#edit"
+        put "/registrations/edit" => "registrations#update"
+        delete "/registrations"  => "registrations#delete"
+        put "/registrations/change_field" => "registrations#change_field"
+        get "/venue" => "venue#show", :as => "venue_info"
+        put "/venue" => "venue#update", :as => "venue_update"
+        get "/dietary_choices" => "dietchoices#show", :as => "dietary_list"
+        put "/dietary_choices" => "dietchoices#update", :as => "dietary_update"
+        get "/volunteers_list" => "volunteers#show"
+        get "/volunteers" => "volunteers#index", :as => "volunteers_info"
+        put "/volunteers" => "volunteers#update", :as => "volunteers_update"
 
-      resources :difficulty_levels, only: [:show, :update, :index]
-
-      resources :rooms, only: [:show, :update, :index]
-
-      resources :tracks, only: [:show, :update, :index]
-
-      resources :eventtypes, only: [:show, :index] do
-        collection do
-          patch :update
+        resources :difficulty_levels, only: [:show, :update, :index]
+        resources :rooms, only: [:show, :update, :index]
+        resources :tracks, only: [:show, :update, :index]
+        resources :eventtypes, only: [:show, :update, :index]
+        resources :social_events, only: [:show, :update, :index]
+        resources :supporter_levels, only: [:show, :update, :index]
+        resources :emails, only: [:show, :update, :index]
+        resources :callforpapers, only: [:show, :update, :index]
+        put "/questions/update_conference" => "questions#update_conference"
+        resources :questions
+        resources :events do
+          member do
+            post :comment
+            put :update_state
+            put :update_track
+            get :vote
+          end
+          resource :speaker, :only => [:edit, :update]
         end
+        resources :supporters
       end
+    end
 
-      resources :social_events, only: [:show, :update, :index]
-
-      resources :supporter_levels, only: [:show, :update, :index]
-
-      resources :emails, only: [:show, :update, :index]
-
-      resources :callforpapers, only: [:create] do
-        collection do
-          patch :update
-          get :show
-        end
+    resources :conference, :only => [] do
+      resources :proposal do
+        resources :event_attachment, :controller => "event_attachments"
+        put "/confirm" => "proposal#confirm"
       end
-
-      patch "/questions/update_conference" => "questions#update_conference"
-      resources :questions
-
-      resources :events do
-        member do
-          post :comment
-          patch :update_state
-          patch :update_track
-          get :vote
-        end
-        resource :speaker, :only => [:edit, :update]
+      resource :schedule, :only => [] do
+        get "/" => "schedule#index"
       end
-
-      resources :supporters
-    end
-  end
-
-  resources :conference, :only => [] do
-    resources :proposal do
-      resources :event_attachment, :controller => "event_attachments"
-      patch "/confirm" => "proposal#confirm"
-    end
-    resource :schedule, :only => [] do
-      get "/" => "schedule#index"
-    end
-    member do
-      get "/register" => "conference_registration#register"
-      patch "/register" => "conference_registration#update"
-      delete "/register" => "conference_registration#unregister"
+      member do
+        get "/register" => "conference_registration#register"
+        put "/register" => "conference_registration#update"
+        delete "/register" => "conference_registration#unregister"
+      end
     end
   end
 
@@ -97,7 +80,11 @@ Osem::Application.routes.draw do
     end
   end
 
-  get "/admin" => redirect("/admin/conference")
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get "/admin" => redirect("/admin/conference")
+  end
 
-  root :to => "home#index"
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root :to => "home#index"
+  end
 end
