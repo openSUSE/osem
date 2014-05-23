98030936fab32a1c715aa089eee5e9be68032afb
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-05-23 21:40:54 +0530

    houndci fixes

--- app/models/supporter_level.rb
+++ app/models/supporter_level.rb
@@ -4,9 +4,7 @@ class SupporterLevel < ActiveRecord::Base
   monetize :price_cents, with_model_currency: :price_currency
   attr_accessible :conference, :title, :url, :description, :price_currency, :amount, :price_cents
   validate :convert_money_to_cents
-  
   private
-  
   def convert_money_to_cents
     self.price_cents = (self.amount.to_d * 100).to_i
   end
