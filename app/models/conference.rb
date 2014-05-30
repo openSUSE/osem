fae5981dc4939a630afc3378b8d28fcb9db8818e
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-05-30 23:52:56 +0530

    Added documentation for the new method added

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -100,20 +100,25 @@ class Conference < ActiveRecord::Base
   # * +false+ -> If the conference dates are not set or today isn't in the
   #   registration period.
   # * +true+ -> If today is in the registration period.
-  def registration_dates_given?
-    if self.registration_start_date.blank? ||  self.registration_end_date.blank?
-      false
-    else
-      true
-    end
-  end
-
   def registration_open?
     today = Date.current
     if registration_dates_given?
       (registration_start_date..registration_end_date).cover?(today)
     else
       false
+    end
+  end
+  ##
+  # Checks if the registration dates for the conference are provided
+  #
+  # ====Returns
+  # * +false+ -> If the conference registration dates are not set
+  # * +true+ -> If conference registration dates are set
+  def registration_dates_given?
+    if self.registration_start_date.blank? ||  self.registration_end_date.blank?
+      false
+    else
+      true
     end
   end
 
