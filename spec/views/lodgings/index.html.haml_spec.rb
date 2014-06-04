4cc039b358fbd6dcbbb4e1cbd6208b27eaf93d17
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-06-04 16:14:54 +0530

    Added lodging backend

---
+++ spec/views/lodgings/index.html.haml_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'admin/lodgings/index' do
+  it 'renders lodgings list' do
+    @conference = create(:conference)
+    @conference.venue = create(:venue)
+    @conference.venue.lodgings << create(:lodging, venue: @conference.venue)
+    assign :venue, @conference.venue
+    render
+    expect(rendered).to include('Example Hotel')
+    expect(rendered).to include('Lorem Ipsum Dolor')
+  end
+end
