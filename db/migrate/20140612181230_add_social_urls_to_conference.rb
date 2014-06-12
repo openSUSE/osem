6d2576421b3d86a0ee2e905b28bdc1b0f3b3f45d
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-06-13 00:20:18 +0530

    Add social url to conference

---
+++ db/migrate/20140612181230_add_social_urls_to_conference.rb
@@ -0,0 +1,7 @@
+class AddSocialUrlsToConference < ActiveRecord::Migration
+  def change
+    add_column :conferences, :twitter_url, :string
+    add_column :conferences, :facebook_url, :string
+    add_column :conferences, :google_url, :string
+  end
+end
