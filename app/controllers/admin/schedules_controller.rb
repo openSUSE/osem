37ec4ce7468a085f33a89bf5b81db88d26dab9ad
Stella Rouzi <differentreality@gmail.com>
2014-06-12 19:49:54 +0300

    fix admin schedule routing

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -1,6 +1,6 @@
-class Admin::ScheduleController < ApplicationController
+class Admin::SchedulesController < ApplicationController
   before_filter :verify_organizer
-  skip_before_filter :verify_authenticity_token, :only => [:update]
+  skip_before_filter :verify_authenticity_token, only: [:update]
   layout "schedule"
 
   def show
@@ -13,26 +13,26 @@ class Admin::ScheduleController < ApplicationController
   end
 
   def update
-    event = Event.where(:guid => params[:event]).first
+    event = Event.where(guid: params[:event]).first
     error_message = nil
     if event.nil?
       error_message = "Could not find event GUID: #{params[:event]}"
     end
 
-    if params[:date] == "none"
+    if params[:date] == 'none'
       event.start_time = nil
       event.room = nil
       event.save!
-      render :json => {"status" => "ok"}
+      render json: {'status' => 'ok'}
       return
     end
-    room = Room.where(:guid => params[:room]).first
+    room = Room.where(guid: params[:room]).first
     if room.nil?
       error_message = "Could not find room GUID: #{params[:room]}"
     end
 
     if !error_message.nil?
-      render :json => {"status" => "error", "message" => error_message}, :status => 500
+      render json: {'status' => 'error', 'message' => error_message}, status: 500
       return
     end
 
@@ -44,10 +44,10 @@ class Admin::ScheduleController < ApplicationController
     # only on output 
     # zone = ActiveSupport::TimeZone::new(@conference.timezone)
     # startTime = DateTime.strptime(time + zone.formatted_offset, "%Y-%m-%d %k:%M %Z")
-    startTime = DateTime.strptime(time, "%Y-%m-%d %k:%M")
+    startTime = DateTime.strptime(time, '%Y-%m-%d %k:%M')
     event.start_time = startTime
     event.save!
-    render :json => {"status" => "ok"}
+    render json: {'status' => 'ok'}
   end
 
 
