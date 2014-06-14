f5c5e6a44b955f5b866a8eba4005f71cfb7e8c90
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-06-14 18:33:39 +0530

    Adds photo to venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,8 +1,13 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :name, :description, :address, :website
+  attr_accessible :name, :description, :website, :address, :photo
   has_many :conferences
   before_create :generate_guid
+  has_attached_file :photo,
+                    styles: { thumb: '100x100>', large: '300x300>' }
 
+  validates_attachment_content_type :photo,
+                                    content_type: [/jpg/, /jpeg/, /png/, /gif/],
+                                    size: { in: 0..500.kilobytes }
   private
 
   def generate_guid
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140612181230) do
+ActiveRecord::Schema.define(version: 20140613083115) do
 
   create_table "answers", force: true do |t|
     t.string   "title"
@@ -390,6 +390,10 @@ ActiveRecord::Schema.define(version: 20140612181230) do
     t.string   "offline_map_bounds"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "versions", force: true do |t|
