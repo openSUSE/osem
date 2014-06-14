feabc3b954b5ca24db46b7881b9532da61143529
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-06-14 22:53:20 +0530

    Adds lodging description to conference

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613083115) do
+ActiveRecord::Schema.define(version: 20140614171853) do
 
   create_table "answers", force: true do |t|
     t.string   "title"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20140613083115) do
     t.string   "google_url"
     t.text     "sponsor_description"
     t.string   "sponsor_email"
+    t.text     "lodging_description"
   end
 
   create_table "conferences_questions", id: false, force: true do |t|
