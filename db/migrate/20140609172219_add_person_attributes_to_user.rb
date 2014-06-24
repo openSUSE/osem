e8990bfb12f22029457271253670d371c14a95af
Stella Rouzi <differentreality@gmail.com>
2014-06-24 12:20:52 +0300

    fix biography field as text, not string

--- db/migrate/20140609172219_add_person_attributes_to_user.rb
+++ db/migrate/20140609172219_add_person_attributes_to_user.rb
@@ -2,7 +2,7 @@ class AddPersonAttributesToUser < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
     add_column :users, :email_public, :boolean
-    add_column :users, :biography, :string
+    add_column :users, :biography, :text
     add_column :users, :nickname, :string
     add_column :users, :affiliation, :string
     add_column :users, :avatar_file_name, :string
