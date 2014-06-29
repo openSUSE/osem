8efcd69f3d37d19f9524276dff490f4a50982111
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-06-29 17:44:27 +0530

    Added hints to description fields to use markdown syntax, rendered all description fields in the splash as markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -198,13 +198,17 @@ module ApplicationHelper
     default_brand
   end
 
-  def dat_markdown(text)
+  def markdown(text)
     options = {
-      :autolink => true,
-      :space_after_headers => true,
-      :no_intra_emphasis => true
+      autolink: true,
+      space_after_headers: true,
+      no_intra_emphasis: true
     }
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     markdown.render(text).html_safe
+  end
+
+  def markdown_hint(text="")
+    markdown("#{text} Please look at #{link_to '**Markdown Syntax**', 'https://daringfireball.net/projects/markdown/syntax', target: '_blank'} to format your text")
   end
 end
