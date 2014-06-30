220e8161559697fcef2e2fabe72119200f3cd203
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-06-30 19:24:26 +0530

    Gallery initiated

--- app/controllers/conference_controller.rb
+++ app/controllers/conference_controller.rb
@@ -3,4 +3,9 @@ class ConferenceController < ApplicationController
     @conference = Conference.find_by_short_title(params[:id])
     not_found unless @conference.make_conference_public?
   end
+
+  def fetch_gallery_photos
+    @photos = Conference.find(params[:id].to_i).photos
+    render 'photos', formats: [:js]
+  end
 end
