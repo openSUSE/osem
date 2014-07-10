6e23c2b45f91a0e87b2587278c291e531749eaca
Gopesh Tulsyan <gopesh.7500@gmail.com>
2014-07-10 21:29:07 +0530

    Removed conference_date_string_helper, moved date_string to application_controller
    Deleted home_helper.rb

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'home/index' do
    it "renders _conference partial for each conference" do
+    allow(view).to receive(:date_string).and_return("January 17 - 21 2014")
     assign(:current, [create(:conference), create(:conference)])
     render
     expect(view).to render_template(:partial => "_conference_details", :count => 2)
