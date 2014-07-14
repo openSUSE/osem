e37e0a50c8ab1e74292992da3b8f2d5fcc143c48
Stella Rouzi <differentreality@gmail.com>
2014-07-14 17:35:03 +0300

    add volunteer fields to user

---
+++ db/migrate/20140714141156_add_volunteer_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddVolunteerFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :mobile, :string
+    add_column :users, :tshirt, :string
+    add_column :users, :languages, :string
+    add_column :users, :volunteer_experience, :text
+  end
+end
