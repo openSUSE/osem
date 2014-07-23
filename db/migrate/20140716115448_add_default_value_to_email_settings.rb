7192e2fe49471bf31b12e9ab20597863f47ba8aa
Chrisbr <cbruckmayer@gmx.net>
2014-07-23 13:20:10 +0200

    Send mail attributes are now on default false

---
+++ db/migrate/20140716115448_add_default_value_to_email_settings.rb
@@ -0,0 +1,21 @@
+class AddDefaultValueToEmailSettings < ActiveRecord::Migration
+  def up
+    change_column :email_settings, :send_on_registration, :boolean, default: false
+    change_column :email_settings, :send_on_accepted, :boolean, default: false
+    change_column :email_settings, :send_on_rejected, :boolean, default: false
+    change_column :email_settings, :send_on_confirmed_without_registration, :boolean, default: false
+    change_column :email_settings, :send_on_updated_conference_dates, :boolean, default: false
+    change_column :email_settings, :send_on_updated_conference_registration_dates, :boolean, default: false
+    change_column :email_settings, :send_on_venue_update, :boolean, default: false
+  end
+
+  def down
+    change_column :email_settings, :send_on_registration, :boolean, default: true
+    change_column :email_settings, :send_on_accepted, :boolean, default: true
+    change_column :email_settings, :send_on_rejected, :boolean, default: true
+    change_column :email_settings, :send_on_confirmed_without_registration, :boolean, default: true
+    change_column :email_settings, :send_on_updated_conference_dates, :boolean, default: true
+    change_column :email_settings, :send_on_updated_conference_registration_dates, :boolean, default: true
+    change_column :email_settings, :send_on_venue_update, :boolean, default: true
+  end
+end
