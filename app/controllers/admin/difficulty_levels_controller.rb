58a63d9fd094444dd2f702b626b47d3644f90260
Stella Rouzi <differentreality@gmail.com>
2014-08-12 15:53:50 +0300

    fix style

--- app/controllers/admin/difficulty_levels_controller.rb
+++ app/controllers/admin/difficulty_levels_controller.rb
@@ -14,18 +14,18 @@ module Admin
             @conference.use_difficulty_levels = false
             @conference.save!
             flash[:error] = "You cannot enable the usage of difficulty levels without having set any levels."
-            redirect_to(admin_conference_difficulty_levels_path(:conference_id => @conference.short_title))
+            redirect_to(admin_conference_difficulty_levels_path(conference_id: @conference.short_title))
           rescue ActiveRecord::RecordInvalid
             flash[:error] = "Something went wrong. Difficulty Levels update failed."
-            redirect_to(admin_conference_difficulty_levels_path(:conference_id => @conference.short_title))
+            redirect_to(admin_conference_difficulty_levels_path(conference_id: @conference.short_title))
           end
         else
           flash[:notice] = "Difficulty Levels were successfully updated."
-          redirect_to(admin_conference_difficulty_levels_path(:conference_id => @conference.short_title))
+          redirect_to(admin_conference_difficulty_levels_path(conference_id: @conference.short_title))
         end
       else
         flash[:error] = "Difficulty Levels update failed."
-        redirect_to(admin_conference_difficulty_levels_path(:conference_id => @conference.short_title))
+        redirect_to(admin_conference_difficulty_levels_path(conference_id: @conference.short_title))
       end
     end
   end
