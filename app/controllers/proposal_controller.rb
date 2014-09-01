42159617b4946085d20c1aba0c34fce316edcf40
Henne Vogelsang <hvogel@opensuse.org>
2014-09-01 18:28:07 +0200

    Fix a double render while confirming proposals

--- app/controllers/proposal_controller.rb
+++ app/controllers/proposal_controller.rb
@@ -115,9 +115,10 @@ class ProposalController < ApplicationController
     if @conference.user_registered?(current_user)
       redirect_to(conference_proposal_index_path(@conference.short_title),
                   notice: 'The proposal was confirmed.')
+    else
+      redirect_to(new_conference_conference_registrations_path(conference_id: @conference.short_title),
+                  alert: 'The proposal was confirmed. Please register to attend the conference.')
     end
-    redirect_to(new_conference_conference_registrations_path(conference_id: @conference.short_title),
-                alert: 'The proposal was confirmed. Please register to attend the conference.')
   end
 
   def restart
