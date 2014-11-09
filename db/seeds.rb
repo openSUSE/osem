e3ad72268eab14de898cda0c31ce094d6fc026b9
Stella <differentreality@gmail.com>
2014-11-09 20:37:28 +0200

    add is_disabled attribute for deleted user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 # Create sample user
 user = User.new(email: 'deleted@localhost.osem', name: 'User deleted', username: 'deleted_user',
-                biography: 'Data is no longer available for deleted user.',
+                biography: 'Data is no longer available for deleted user.', is_disabled: true,
                 password: Devise.friendly_token[0, 20])
 user.skip_confirmation!
 user.save!
