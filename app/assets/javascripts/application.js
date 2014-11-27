c84ebf26e008a0492fb2d7f3f05b4fa0c4d2b975
Henne Vogelsang <hvogel@opensuse.org>
2014-11-27 14:00:03 +0100

    Better scroll handling for the venue map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require waypoints
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require cocoon
