6d0f3021ca82a8a7f44f9c450456879490349636
Stella Rouzi <differentreality@gmail.com>
2014-12-01 14:40:26 +0200

    rubocop styling

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   protected
 
-  def after_confirmation_path_for(resource_name, resource)
+  def after_confirmation_path_for(_resource_name, resource)
     if signed_in?
       signed_in_root_path(resource)
     else
