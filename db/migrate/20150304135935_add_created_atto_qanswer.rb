c7271780f2fea988720c2138bae6cfbfd12b7fa5
Henne Vogelsang <hvogel@opensuse.org>
2015-03-04 15:10:11 +0100

    Add creation/update times to qanswers

---
+++ db/migrate/20150304135935_add_created_atto_qanswer.rb
@@ -0,0 +1,6 @@
+class AddCreatedAttoQanswer < ActiveRecord::Migration
+  def change
+    add_column :qanswers, :created_at, :datetime
+    add_column :qanswers, :updated_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -277,6 +277,8 @@ ActiveRecord::Schema.define(version: 20141130182139) do
   create_table "qanswers", force: true do |t|
     t.integer "question_id"
     t.integer "answer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "qanswers_registrations", id: false, force: true do |t|
