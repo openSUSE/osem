68bbdb9fc9a54484aa7894628641fb98ee0d707e
Björn Geuken <bgeuken@suse.de>
2015-05-05 14:04:26 +0200

    Fix styling issue: " -> '

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,14 +6,14 @@ describe Event do
     it 'counts words in abstract' do
       event = build(:event)
       expect(event.abstract_word_count).to eq(233)
-      event.update_attributes!(abstract: "abstract.")
+      event.update_attributes!(abstract: 'abstract.')
       expect(event.abstract_word_count).to eq(1)
     end
 
     it 'counts 0 when abstract is empty' do
       event = build(:event, abstract: nil)
       expect(event.abstract_word_count).to eq(0)
-      event.abstract = ""
+      event.abstract = ''
       expect(event.abstract_word_count).to eq(0)
     end
   end
