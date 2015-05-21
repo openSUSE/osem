7638b33926caa3b4062db85de80927e20d19c1e1
Björn Geuken <bgeuken@suse.de>
2015-05-21 18:48:07 +0200

    Fix registration_dates_given?
    When the registration period is nil registration_dates_given? should return
    false.

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -155,7 +155,7 @@ class Conference < ActiveRecord::Base
   # * +false+ -> If the conference registration dates are not set
   # * +true+ -> If conference registration dates are set
   def registration_dates_given?
-    if registration_period && (registration_period.start_date.blank? || registration_period.end_date.blank?)
+    if registration_period.nil? || (registration_period.start_date.blank? || registration_period.end_date.blank?)
       false
     else
       true
