9651c57748ae30991450cc427b39b92e7650278e
Sarah Julia Kriesch <skriesch@users.noreply.github.com>
2015-05-31 22:03:35 +0200

    Update 201505312045_rename_email_settings.rb
    The class has got the name rename_email_settings now.

--- db/migrate/201505312045_rename_email_settings.rb
+++ db/migrate/201505312045_rename_email_settings.rb
@@ -1,10 +1,10 @@
-class FixColumnNames < ActiveRecord::Migration
+class rename_email_settings < ActiveRecord::Migration
   def up
     change_table :email_settings do |t|
       t.rename :registration_email_template, :registration_body
       t.rename :accepted_email_template, :accepted_body
-	t.rename :rejected_email_template, :rejected_body
-	t.rename :confirmed_email_template, :confirmed_body
+      t.rename :rejected_email_template, :rejected_body
+      t.rename :confirmed_email_template, :confirmed_body
     end
   end
 
@@ -12,8 +12,8 @@ class FixColumnNames < ActiveRecord::Migration
     change_table :email_settings do |t|
       t.rename :registration_body, :registration_email_template
       t.rename :accepted_body, :accepted_email_template
-	t.rename :rejected_body, :rejected_email_template
-	t.rename :confirmed_body, :confirmed_email_template
+      t.rename :rejected_body, :rejected_email_template
+      t.rename :confirmed_body, :confirmed_email_template
     end
   end
 end
