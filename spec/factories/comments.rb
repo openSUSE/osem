eab2894fad39b9db11b390a6cc7cd5ef387cefe1
raluka <badoi.raluca@gmail.com>
2015-08-25 14:41:09 +0200

    created comments with FactoryGirl

---
+++ spec/factories/comments.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :comment do
+    body 'Most interresting comment ever, created by a girl.'
+    user
+    association :commentable, factory: :event
+  end
+end
