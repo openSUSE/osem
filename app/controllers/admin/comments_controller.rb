800a41b3f9726c2a28d045e7c1624ed63e26b453
raluka <badoi.raluca@gmail.com>
2015-09-03 12:17:42 +0200

    added logic for available conferences based on roles in comments_controller.rb

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,11 +1,9 @@
 module Admin
   class CommentsController < Admin::BaseController
-    load_and_authorize_resource :conference, find_by: :short_title
     load_and_authorize_resource
 
-
     def index
-      @ordered_events = Event.order(:title).all
+      @conferences_available = Conference.with_roles([:admin, :organizer, :cfp], current_user)
     end
   end
 end
