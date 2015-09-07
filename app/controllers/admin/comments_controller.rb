1addae9cd992ef02243883266bc4561b36fa7e07
raluka <badoi.raluca@gmail.com>
2015-09-07 18:56:26 +0200

    WIP changed CommentsController#index; temporary view for all_comments

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,7 +3,8 @@ module Admin
     load_and_authorize_resource
 
     def index
-      @conferences_available = Conference.with_roles([:admin, :organizer, :cfp], current_user)
+      @comments = Comment.accessible_by(current_ability).order(created_at: :desc).group_by { |comment| comment.commentable.conference }
+      @another_comments = Hash[@comments.map{|conference, comments| [conference, comments.group_by {|comment| comment.commentable}]}]
     end
   end
 end
