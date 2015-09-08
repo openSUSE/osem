b762b82e74775a57ecce33e731b609ad940675bc
raluka <badoi.raluca@gmail.com>
2015-09-08 12:15:22 +0200

    rewrite CommentsController#index to have everything in one call

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,8 +3,7 @@ module Admin
     load_and_authorize_resource
 
     def index
-      @comments = Comment.accessible_by(current_ability).order(created_at: :desc).group_by { |comment| comment.commentable.conference }
-      @another_comments = Hash[@comments.map{|conference, comments| [conference, comments.group_by {|comment| comment.commentable}]}]
+      @comments = Comment.accessible_by(current_ability).order(created_at: :desc).group_by { |comment| comment.commentable.conference }.map {|conference, comments| [conference, comments.group_by {|comment| comment.commentable}]}.to_h
     end
   end
 end
