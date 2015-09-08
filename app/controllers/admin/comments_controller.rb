932cb9984f951690016654be2493b8336e22388f
raluka <badoi.raluca@gmail.com>
2015-09-08 14:27:14 +0200

    sorting comments after event title and created at in index

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,7 +3,7 @@ module Admin
     load_and_authorize_resource
 
     def index
-      @comments = Comment.accessible_by(current_ability).order(created_at: :desc).group_by { |comment| comment.commentable.conference }.map {|conference, comments| [conference, comments.group_by {|comment| comment.commentable}]}.to_h
+      @comments = Comment.accessible_by(current_ability).joins('INNER JOIN events ON commentable_id = events.id').order('events.title', 'comments.created_at DESC').group_by{ |comment| comment.commentable.conference }.map {|conference, comments| [conference, comments.group_by{|comment| comment.commentable}]}.to_h
     end
   end
 end
