d365d5efa1c465eb2468c2b4d35c6988f3afbee1
raluka <badoi.raluca@gmail.com>
2015-09-09 16:50:07 +0200

    added test file for comments_controller WIP

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -12,8 +12,8 @@ module Admin
       Comment.accessible_by(current_ability).joins('INNER JOIN events ON commentable_id = events.id').order('events.title', 'comments.created_at DESC')
     end
 
-    def grouped_comments(comments)
-      comments.group_by{ |comment| comment.commentable.conference }.map {|conference, comments| [conference, comments.group_by{|comment| comment.commentable}]}.to_h
+    def grouped_comments(remarks)
+      remarks.group_by{ |comment| comment.commentable.conference }.map {|conference, comments| [conference, comments.group_by{|comment| comment.commentable}]}.to_h
     end
   end
 end
