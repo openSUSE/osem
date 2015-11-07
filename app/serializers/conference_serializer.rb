6be8d383b7e3794af385492912233e8a071e50cf
Stella Rouzi <differentreality@gmail.com>
2015-11-07 22:55:09 +0200

    Remove unnecessary code

--- app/serializers/conference_serializer.rb
+++ app/serializers/conference_serializer.rb
@@ -65,21 +65,9 @@ class ConferenceSerializer < ActiveModel::Serializer
     object.revision || 0
   end
 
-  # FIXME: adjusting the format the DIRTY way, for oSC13.
-  # If you think this is ugly, don't look at the methods below
   def date_range
     if defined? object.date_range_string
         object.date_range_string.try(:split, ',').try(:first)
     end
   end
-
-#   # FIXME: just giving suseconferenceclient something to play with
-#   def description
-#     'openSUSE Conference 2013 - Power to the Geeko'
-#   end
-#
-#   # FIXME: same than the former
-#   def url
-#     'https://conference.opensuse.org/'
-#   end
 end
