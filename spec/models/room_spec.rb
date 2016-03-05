69a4139268fbab18f98c2e9cc0b97b8aa0ffa411
Aditya Prakash <aditya.prakash132@gmail.com>
2016-03-05 16:41:46 +0530

    Tests for room model

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Room do
+  subject { create(:room) }
+
+  describe 'validation' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:venue_id) }
+    it { should validate_numericality_of(:size).only_integer.is_greater_than(0).allow_nil }
+  end
+
+  describe 'association' do
+    it { should belong_to(:venue) }
+    it { should have_many(:events).dependent(:nullify) }
+  end
+
+  describe 'callback' do
+    after { subject.run_callbacks(:create) }
+
+    it '#generate_guid' do
+      regex_base64 = %r{^(?:[A-Za-z_\-0-9+\/]{4}\n?)*(?:[A-Za-z_\-0-9+\/]{2}|[A-Za-z_\-0-9+\/]{3}=)?$}
+      expect(subject).to receive(:generate_guid)
+      expect(subject.guid).to match regex_base64
+    end
+  end
+end
