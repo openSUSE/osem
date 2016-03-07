483d9a234e1fbe04bdf227851551040096674a6d
Carlos Coelho <carlos@pencillabs.com>
2016-03-07 09:43:55 -0300

    Verified contact has email or not
    issue #764
    
    Signed-off-by: Carlos Coelho <carlos@pencillabs.com>

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < ActiveRecord::Base
             format: URI::regexp(%w(http https)), allow_blank: true
 
   def has_social_media?
-    return true if !facebook.blank? || !twitter.blank? || !googleplus.blank? || !instagram.blank?
+    return true if !facebook.blank? || !twitter.blank? || !googleplus.blank? || !instagram.blank? || !email.blank?
     false
   end
 end
