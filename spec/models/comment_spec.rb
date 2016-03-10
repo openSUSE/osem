a6c89351c5b6c6cafd9377a75623ec8390d3add6
Christian Bruckmayer <christian@bruckmayer.net>
2016-03-10 13:59:59 +0100

    Make Comment find_since_last_login scope robust against nil value
    to fix production ArgumentError: bad value for range. Fixes #807.

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Commercial do
+
+  describe '.find_since_last_login' do
+    it 'returns none if last_sign_in_at is nil' do
+      expect(Comment.find_since_last_login(create(:user))).to eq(Comment.none)
+    end
+  end
+end
