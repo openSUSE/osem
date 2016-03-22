bd36ce0fb2291866d1efb2a56c44493d5398b497
Henne Vogelsang <hvogel@opensuse.org>
2016-03-22 17:53:49 +0100

    Switch logic for linting factories
    Only if you set the environment variable OSEM_FACTORY_LINT
    to 'false' it won't lint.

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,7 +3,7 @@ require_relative 'external_request'
 RSpec.configure do |config|
 
   config.before(:suite) do
-    if CONFIG['factory_girl_lint']
+    if ENV['OSEM_FACTORY_LINT'] != 'false'
       mock_commercial_request
       FactoryGirl.lint
     end
