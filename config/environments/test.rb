fee8adba4bb718ca427d85cf09eb1a9acd1d4f8c
Henne Vogelsang <hvogel@opensuse.org>
2016-03-23 11:49:30 +0100

    Also set a default secret_key_base in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,9 @@ Osem::Application.configure do
   # Do not perform deliveries on test
   config.action_mailer.perform_deliveries = false
 
+  # Set the secret key base if it's not set via other means
+  config.secret_key_base ||= 'f4be765bc98e516de82ac01daa8f8aa11c5ca13cb6c911887851ac89457b6c0b056b2361a21b5c08926c9386e0f91eef84fc0b103d522bf00bc0c78ea8ce7c58'
+
   config.after_initialize do
     ActiveRecord::Base.logger = nil
     # Set Time.now to May 1, 2014 00:01:00 AM (at this instant), but allow it to move forward
