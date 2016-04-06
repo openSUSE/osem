5cdedbf9a6f818b5a142a0f89a523bf9ce17af57
Shriyansh <shriyansh.agrawal@research.iiit.ac.in>
2016-04-06 20:13:01 +0530

    rubocop issues solved

--- app/controllers/admin/venue_commercials_controller.rb
+++ app/controllers/admin/venue_commercials_controller.rb
@@ -1,15 +1,14 @@
 module Admin
   class VenueCommercialsController < Admin::BaseController
     load_and_authorize_resource :conference, find_by: :short_title
-    load_and_authorize_resource :venue ,through: :conference, singelton: true
+    load_and_authorize_resource :venue, through: :conference, singelton: true
     before_action :set_venue
-    before_action :set_commercial , only: [:update , :destroy]
+    before_action :set_commercial, only: [:update, :destroy]
 
     def create
       @commercial = @venue.build_commercial(commercial_params)
       authorize! :create, @commercial
 
-      #FIXME redirect to tab#commercial
       if @commercial.save
         redirect_to edit_admin_conference_venue_path,
                     notice: 'Commercial was successfully created.'
@@ -51,11 +50,11 @@ module Admin
     def commercial_params
       params.require(:commercial).permit(:url)
     end
-    
+
     def set_venue
       @venue = @conference.venue
     end
-    
+
     def set_commercial
       @commercial = Commercial.find params[:id]
     end
