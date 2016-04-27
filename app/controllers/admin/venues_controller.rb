2a4de3e8694c94b50e287cccf00b9b274f79305c
Henne Vogelsang <hvogel@opensuse.org>
2016-04-27 15:59:34 +0200

    Use PictureUploader on Venue

--- app/controllers/admin/venues_controller.rb
+++ app/controllers/admin/venues_controller.rb
@@ -44,7 +44,7 @@ module Admin
     private
 
     def venue_params
-      params.require(:venue).permit(:name, :street, :postalcode, :city, :country, :longitude, :latitude, :description, :website, :photo, :lodgings_attributes, :conference_id)
+      params.require(:venue).permit(:name, :street, :postalcode, :city, :country, :longitude, :latitude, :description, :website, :picture, :picture_cache, :lodgings_attributes, :conference_id)
     end
   end
 end
