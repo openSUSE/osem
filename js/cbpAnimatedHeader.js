b95db4c8adb6dbae1c696eadee896527e6a5cfc3
Henne Vogelsang <hvogel@opensuse.org>
2016-05-20 13:27:58 +0200

    Use logo as brand image

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -12,6 +12,7 @@ var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
+		logo = document.querySelector( '.osem-logo' ),
 		didScroll = false,
 		changeHeaderOn = 300;
 
@@ -28,9 +29,13 @@ var cbpAnimatedHeader = (function() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
 			classie.add( header, 'navbar-shrink' );
+			classie.add( logo, 'osem-logo-shrink' );
+			classie.remove( logo, 'osem-logo' );
 		}
 		else {
 			classie.remove( header, 'navbar-shrink' );
+			classie.add( logo, 'osem-logo' );
+			classie.remove( logo, 'osem-logo-shrink' );
 		}
 		didScroll = false;
 	}
