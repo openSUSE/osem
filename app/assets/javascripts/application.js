82c2c400b6677c4afd6581a0f9028e768f38e8f5
Ana <anamma06@gmail.com>
2016-06-16 13:04:44 +0200

    Swipe support added to the carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.mobile.custom.min
 //= require waypoints/jquery.waypoints
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
