14594c7a4dc661831d07d6b97bfd69d7032a4ddd
Rishabh Saxena <orishabh1@gmail.com>
2016-06-18 18:30:17 +0530

    add payment views, styling and its helper methods

---
+++ app/helpers/payments_helper.rb
@@ -0,0 +1,13 @@
+module PaymentsHelper
+  def months
+    (1..12).collect{|n| ["#{n} - #{Date::MONTHNAMES[n]}", n]}
+  end
+
+  def years
+    (Date.current.year..Date.current.year + 15)
+  end
+
+  def card_types
+    [[['Visa', 'visa'], ['MasterCard', 'master'], ['Discover', 'discover'], ['American Express', 'american_express']]]
+  end
+end
