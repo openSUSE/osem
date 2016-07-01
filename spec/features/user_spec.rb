3eeba6cbb044d9af8d531b32c4dab819a16ee5ac
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-07-02 00:38:43 +0530

    Allow admins to manually confirm users

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,8 +2,23 @@ require 'spec_helper'
 
 feature User do
 
-  shared_examples 'admin ability' do
+  let(:admin) { create(:admin) }
 
+  shared_examples 'admin ability' do
+    scenario 'manually confirm unconfirmed users', feature: true, js: true do
+      sign_in admin
+      user = create(:user, confirmed_at: nil)
+      expect(user.confirmed?).to be false
+      visit admin_users_path
+
+      within('tr', text: user.name) do
+        expect(page.has_content?('unconfirmed')).to be true
+        click_link 'Confirm'
+      end
+
+      user.reload
+      expect(user.confirmed?).to be true
+    end
   end
 
   describe 'admin' do
