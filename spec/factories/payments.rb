52c30af6425b56f5c1bc7cbddf5ba85ac439e0b0
Rishabh Saxena <orishabh1@gmail.com>
2016-07-28 15:28:43 +0530

    generalise application icon for Stripe iFrame

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :payment do
     user
     conference
-    last4 4242
-    authorization_code 1234567890
+    last4 '4242'
+    authorization_code '1234567890'
     amount 10
   end
 end
