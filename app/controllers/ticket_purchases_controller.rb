261d7dd0deb0bb4bada3f4fa86b13c8b6086edcd
Rishabh Saxena <orishabh1@gmail.com>
2016-08-11 17:47:52 +0530

    improve flash error collection
    improve views

--- app/controllers/ticket_purchases_controller.rb
+++ app/controllers/ticket_purchases_controller.rb
@@ -4,7 +4,7 @@ class TicketPurchasesController < ApplicationController
   authorize_resource :conference_registrations, class: Registration
 
   def create
-    TicketPurchase.by_conference(@conference).unpaid.by_user(current_user).destroy_all
+    current_user.ticket_purchases.by_conference(@conference).unpaid.destroy_all
     message = TicketPurchase.purchase(@conference, current_user, params[:tickets][0])
     if message.blank?
       if current_user.ticket_purchases.by_conference(@conference).unpaid.any?
