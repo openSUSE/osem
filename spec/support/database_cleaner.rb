244172a9bce9586b3c37bf8852c130c77d866b5d
Hernan Schmidt <hschmidt@suse.com>
2016-08-16 19:02:59 +0200

    Clean up DatabaseCleaner usage

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -10,12 +10,4 @@ RSpec.configure do |config|
   config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
