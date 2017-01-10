ecd80580053b895d451fe46ff066ba2843032476
Eugene Dubinin <eugend@commandprompt.com>
2017-01-10 17:07:28 +0200

    implement user creation by admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,17 @@ module Admin
       @user = User.new
     end
 
+    def create
+      @user = User.new(user_params)
+      @user.skip_confirmation!
+      if @user.save
+        redirect_to admin_users_path, notice: 'User successfully created.'
+      else
+        flash.now[:error] = "Creating User failed: #{@user.errors.full_messages.join('. ')}."
+        render :new
+      end
+    end
+
     def index
       @users = User.all
     end
@@ -50,8 +61,9 @@ module Admin
     private
 
     def user_params
-      params.require(:user).permit(:email, :name, :email_public, :biography, :nickname, :affiliation, :is_admin, :username, :login, :is_disabled,
-                                   :tshirt, :mobile, :volunteer_experience, :languages, :to_confirm, role_ids: [])
+      params.require(:user).permit(:email, :name, :email_public, :biography, :nickname, :affiliation, :is_admin,
+                                   :username, :login, :is_disabled, :tshirt, :mobile, :volunteer_experience,
+                                   :languages, :to_confirm, :password, role_ids: [])
     end
   end
 end
