ec1adbada8ba36cfea53b7640d08e24e3472080f
gotens1211 <nikeythe@gmail.com>
2017-02-23 15:20:53 +0530

    Removed duplicate methods date_range_string and date_string.
    Added tests for date_string function
    
    corrections in the test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :get_conferences
   before_filter :store_location
-  helper_method :date_string
   # Ensure every controller authorizes resource or skips authorization (skip_authorization_check)
   check_authorization unless: :devise_controller?
 
@@ -67,40 +66,5 @@ class ApplicationController < ActionController::Base
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
-  end
-
-  ##
-  # Returns a string build from the start and end date of the given conference.
-  #
-  # If the conference is only one day long
-  # * %B %d %Y (January 17 2014)
-  # If the conference starts and ends in the same month and year
-  # * %B %d - %d, %Y (January 17 - 21 2014)
-  # If the conference ends in another month but in the same year
-  # * %B %d - %B %d, %Y (January 31 - February 02 2014)
-  # All other cases
-  # * %B %d, %Y - %B %d, %Y (December 30, 2013 - January 02, 2014)
-  def date_string(start_date, end_date)
-    startstr = 'Unknown - '
-    endstr = 'Unknown'
-    # When the conference  in the same month
-    if start_date.month == end_date.month && start_date.year == end_date.year
-      if start_date.day == end_date.day
-        startstr = start_date.strftime('%B %d')
-        endstr = end_date.strftime(' %Y')
-      else
-        startstr = start_date.strftime('%B %d - ')
-        endstr = end_date.strftime('%d, %Y')
-      end
-    elsif start_date.month != end_date.month && start_date.year == end_date.year
-      startstr = start_date.strftime('%B %d - ')
-      endstr = end_date.strftime('%B %d, %Y')
-    else
-      startstr = start_date.strftime('%B %d, %Y - ')
-      endstr = end_date.strftime('%B %d, %Y')
-    end
-
-    result = startstr + endstr
-    result
   end
 end
