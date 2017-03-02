dcfc25a541aa54f516952661563c348530fc1361
Siddhant Bajaj <sbajaj1997@gmail.com>
2017-03-02 17:07:19 +0530

    Show vote versions in event history

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -215,6 +215,7 @@ class Ability
     end
 
     can [:index, :revert_object, :revert_attribute], PaperTrail::Version, item_type: 'Event', conference_id: conf_ids_for_cfp
+    can [:index, :revert_object, :revert_attribute], PaperTrail::Version, item_type: 'Vote', conference_id: conf_ids_for_cfp
     can [:index, :revert_object, :revert_attribute], PaperTrail::Version do |version|
       version.item_type == 'Commercial' && conf_ids_for_cfp.include?(version.conference_id) &&
       (version.object.to_s.include?('Event') || version.object_changes.to_s.include?('Event'))
