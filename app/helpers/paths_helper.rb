e51c73d9cfa4390a02e6bbbfc6e03b88d086a02a
hitman <agrimmittal97@gmail.com>
2017-03-07 01:42:33 +0530

    add documentation for each helper

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -30,4 +30,13 @@ module PathsHelper
       return ''
     end
   end
+
+  # Same as redirect_to(:back) if there is a valid HTTP referer, otherwise redirect_to()
+  def redirect_back_or_to(options = {}, response_status = {})
+    if request.env['HTTP_REFERER']
+      redirect_to :back
+    else
+      redirect_to options, response_status
+    end
+  end
 end
