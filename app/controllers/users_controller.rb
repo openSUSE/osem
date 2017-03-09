18d0f6215b5d0c9ff2f95170237ded2c1b02eda9
Chaitanya <cayadav2009@gmail.com>
2017-03-10 00:36:22 +0530

    Fixes #1081, Add more profile info on user Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,25 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @tab_select = params[:tab]
+    if @tab_select.present?
+      respond_to do |format|
+        format.js { render action: 'field_select' }
+      end
+    else
+      render :edit
+    end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
-    else
-      flash[:error] = "An error prohibited your Profile from being saved: #{@user.errors.full_messages.join('. ')}."
-      render :edit
+    respond_to do |format|
+      if @user.update(user_params)
+        flash[:notice] = 'User was successfully updated.'
+      else
+        flash[:error] = "An error prohibited your Profile from being saved: #{@user.errors.full_messages.join('. ')}."
+      end
+      format.js
     end
   end
 
@@ -24,6 +34,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :biography, :nickname, :affiliation)
+      params.require(:user).permit(:name, :biography, :nickname, :affiliation, :googleplus, :twitter, :gna, :gnu, :github, :gitlab, :website_url, :linkedin, :diaspora, :savannah)
     end
 end
