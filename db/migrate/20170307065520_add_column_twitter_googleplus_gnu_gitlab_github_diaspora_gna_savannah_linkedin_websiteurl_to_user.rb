7ea168fef4648b3cb008c396bcbf6e83e2c3a1e0
Chaitanya <cayadav2009@gmail.com>
2017-03-10 00:35:30 +0530

    Add column for user's social media and social code sites info

---
+++ db/migrate/20170307065520_add_column_twitter_googleplus_gnu_gitlab_github_diaspora_gna_savannah_linkedin_websiteurl_to_user.rb
@@ -0,0 +1,14 @@
+class AddColumnTwitterGoogleplusGnuGitlabGithubDiasporaGnaSavannahLinkedinWebsiteurlToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :twitter, :string
+    add_column :users, :googleplus, :string
+    add_column :users, :github, :string
+    add_column :users, :gitlab, :string
+    add_column :users, :diaspora, :string
+    add_column :users, :linkedin, :string
+    add_column :users, :website_url, :string
+    add_column :users, :savannah, :string
+    add_column :users, :gna, :string
+    add_column :users, :gnu, :string
+  end
+end
