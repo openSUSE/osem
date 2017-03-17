dbffece956dc45fc2924bf0466ba0fc4928ce295
hitman <agrimmittal97@gmail.com>
2017-03-17 23:05:51 +0530

    remove !! from can_manage_volunteers

--- app/helpers/admin/volunteers_helper.rb
+++ app/helpers/admin/volunteers_helper.rb
@@ -1,7 +1,11 @@
 module Admin
   module VolunteersHelper
     def can_manage_volunteers?(conference)
-      !!(current_user.has_role? :organizer, conference) || (current_user.has_role? :volunteers_coordinator, conference)
+      if (current_user.has_role? :organizer, conference) || (current_user.has_role? :volunteers_coordinator, conference)
+        true
+      else
+        false
+      end
     end
   end
 end
