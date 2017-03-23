d06309459e94d3374b23a0b51f5018283b651f33
Agrim Mittal <agrimmittal97@gmail.com>
2017-03-24 04:12:25 +0530

    shift relevant functions to versions_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -27,24 +27,4 @@ module UsersHelper
   def show_roles(roles)
     roles.map{ |x| x[0].titleize + ' (' + x[1].join(', ') + ')' }.join ', '
   end
-
-  def user_change_description(version)
-    if version.event == 'create'
-      link_to_user(version.item_id) + ' signed up'
-    elsif version.event == 'update'
-      if version.changeset.keys.include?('reset_password_sent_at')
-        'Someone requested password reset of'
-      elsif version.changeset.keys.include?('confirmed_at') && version.changeset['confirmed_at'][0].nil?
-        (version.whodunnit.nil? ? link_to_user(version.item_id) : link_to_user(version.whodunnit)) + ' confirmed account of'
-      elsif version.changeset.keys.include?('confirmed_at') && version.changeset['confirmed_at'][1].nil?
-        link_to_user(version.whodunnit) + ' unconfirmed account of'
-      else
-        link_to_user(version.whodunnit) + " updated #{updated_attributes(version)} of"
-      end
-    end
-  end
-
-  def users_role_change_description(version)
-    version.event == 'create' ? 'added' : 'removed'
-  end
 end
