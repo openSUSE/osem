3685076b4c476dedac29f59f0bf4359b79fe2a81
AEtherC0r3 <master@aetherc0r3.eu>
2017-06-08 12:25:33 +0300

    Implement track requests and add the track organizer role
    About track requests:
    Create migration that adds the fields submitter_id, state, and
    cfp_active to Tracks
    Add validations and the self_organized? method to the Track model
    Create a new TracksController outide of the admin namespace
    Create the relevant views for index, show, new and edit
    Modify the admin views for tracks to include extra info for
    self-organized tracks
    
    About track organizers:
    Create the role when a self-organized track is created
    Define track organizer abilities
    Modify the roles views and controller to handle the new role
    
    The route for Roles#edit needs to have higher priority than the nested
    routes for track roles, otherwise, the word edit in the url is matched
    as a track with short_name edit

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,47 @@
+class TracksController < ApplicationController
+  load_resource :conference, find_by: :short_title
+  load_resource :program, through: :conference, singleton: true
+  load_and_authorize_resource through: :program, find_by: :short_name
+
+  def index
+    @tracks = current_user.tracks.where(program: @program)
+  end
+
+  def show; end
+
+  def new
+    @track = @program.tracks.new(color: @conference.next_color_for_collection(:tracks))
+  end
+
+  def edit; end
+
+  def create
+    @track = @program.tracks.new(track_params)
+    @track.submitter = current_user
+    @track.state = 'new'
+    @track.cfp_active = false
+    if @track.save
+      redirect_to conference_program_tracks_path(conference_id: @conference.short_title),
+                  notice: 'Track request successfully created.'
+    else
+      flash.now[:error] = "Creating Track request failed: #{@track.errors.full_messages.join('. ')}."
+      render :new
+    end
+  end
+
+  def update
+    if @track.update_attributes(track_params)
+      redirect_to admin_conference_program_tracks_path(conference_id: @conference.short_title),
+                  notice: 'Track request successfully updated.'
+    else
+      flash.now[:error] = "Track request update failed: #{@track.errors.full_messages.join('. ')}."
+      render :edit
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:name, :description, :color, :short_name)
+  end
+end
---
+++ db/migrate/20170705075039_add_state_cfp_active_and_submitter_reference_to_tracks.rb
@@ -0,0 +1,8 @@
+class AddStateCfpActiveAndSubmitterReferenceToTracks < ActiveRecord::Migration
+  def change
+    add_column :tracks, :state, :string
+    add_column :tracks, :cfp_active, :boolean
+    add_column :tracks, :submitter_id, :integer
+    add_index :tracks, :submitter_id
+  end
+end
