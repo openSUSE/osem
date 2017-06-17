65edb997f9795658f6d742bb237b031dc319d81e
AEtherC0r3 <master@aetherc0r3.eu>
2017-06-18 02:57:39 +0300

    Update Ruby to 2.2
    Force development environment to use ruby 2.2 in preparation for the
    update to Rails 5

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 pushd /vagrant
 
 echo -e "\ninstalling required software packages...\n"
-zypper -q -n install update-alternatives ruby-devel make gcc gcc-c++ \
+zypper -q -n install update-alternatives ruby2.2-devel make gcc gcc-c++ \
              libxml2-devel libxslt-devel nodejs screen mariadb \
              libmysqld-devel sqlite3-devel imagemagick
 
@@ -10,7 +10,7 @@ echo -e "\ndisabling versioned gem binary names...\n"
 echo 'install: --no-format-executable' >> /etc/gemrc
 
 echo -e "\ninstalling bundler...\n"
-gem install bundler
+gem.ruby2.2 install bundler
 
 echo -e "\ninstalling your bundle...\n"
 su - vagrant -c "cd /vagrant/; bundle install --quiet"
