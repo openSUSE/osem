c066aadae2138961952a1495d2afa7953ef2e539
siddhantbajaj <sbajaj1997@gmail.com>
2017-07-11 17:05:07 +0530

    Added TicketScanning Model

--- app/models/physical_ticket.rb
+++ app/models/physical_ticket.rb
@@ -3,4 +3,5 @@ class PhysicalTicket < ActiveRecord::Base
   has_one :ticket, through: :ticket_purchase
   has_one :conference, through: :ticket_purchase
   has_one :user, through: :ticket_purchase
+  has_many :ticket_scannings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629232817) do
+ActiveRecord::Schema.define(version: 20170711102511) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.uuid     "visit_id",   limit: 16
@@ -466,6 +466,12 @@ ActiveRecord::Schema.define(version: 20170629232817) do
     t.integer  "user_id"
     t.integer  "payment_id"
     t.integer  "week"
+  end
+
+  create_table "ticket_scannings", force: :cascade do |t|
+    t.integer  "physical_ticket_id", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "tickets", force: :cascade do |t|
