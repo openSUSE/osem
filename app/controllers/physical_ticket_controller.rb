e2a4cfd9fd5a9e2a47da9656ed734f58c07b1e5c
siddhantbajaj <sbajaj1997@gmail.com>
2017-07-15 03:37:32 +0530

    Added ticket pdf prawn document

--- app/controllers/physical_ticket_controller.rb
+++ app/controllers/physical_ticket_controller.rb
@@ -13,5 +13,15 @@ class PhysicalTicketController < ApplicationController
     @file_name = "ticket_for_#{@conference.short_title}"
     @user = @physical_ticket.user
     @ticket_layout = @conference.ticket_layout.to_sym
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = TicketPdf.new(@conference, @user, @physical_ticket, @ticket_layout, @file_name)
+        send_data pdf.render,
+                  filename: @file_name,
+                  type: 'application/pdf',
+                  disposition: 'attachment'
+      end
+    end
   end
 end
