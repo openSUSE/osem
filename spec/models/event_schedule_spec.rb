c4eec6a3de27041162db476bed3f3b667013b59b
AEtherC0r3 <master@aetherc0r3.eu>
2017-08-08 16:18:11 +0300

    Validate room and start_time for EventSchedule
    Don't allow an event to be scheduled outside of it's track's room and
    time slot

--- spec/models/event_schedule_spec.rb
+++ spec/models/event_schedule_spec.rb
@@ -45,5 +45,68 @@ describe EventSchedule do
         end
       end
     end
+
+    describe '#room_of_track' do
+      before :each do
+        conference = create(:conference)
+        conference.venue = create(:venue)
+        @room = create(:room, venue: conference.venue)
+        @track = create(:track, program: conference.program, room: @room)
+        @event = create(:event, program: conference.program, track: @track)
+      end
+
+      context 'is valid' do
+        it 'when scheduled in the track\'s room' do
+          event_schedule = build(:event_schedule, event: @event, room: @room)
+          expect(event_schedule.valid?).to eq true
+        end
+
+        it 'when the track doesn\'t have a room' do
+          @track.room = nil
+          @track.save!
+          event_schedule = build(:event_schedule, event: @event)
+          expect(event_schedule.valid?).to eq true
+        end
+      end
+
+      context 'is invalid' do
+        it 'when scheduled in different room than the track\'s' do
+          event_schedule = build(:event_schedule, event: @event)
+          expect(event_schedule.valid?).to eq false
+          expect(event_schedule.errors[:room]).to eq ["must be the same as the track's room (#{@room.name})"]
+        end
+      end
+    end
+
+    describe '#during_track' do
+      before :each do
+        conference = create(:conference, start_date: Date.current - 1.day, start_hour: 0, end_hour: 24)
+        conference.venue = create(:venue)
+        @room = create(:room, venue: conference.venue)
+        @track = create(:track, program: conference.program, room: @room, start_date: Date.current, end_date: Date.current)
+        @event = create(:event, program: conference.program, track: @track)
+      end
+
+      context 'is valid' do
+        it 'when scheduled during the track\'s time slot' do
+          event_schedule = build(:event_schedule, event: @event, room: @room, start_time: Date.current + 3.hours)
+          expect(event_schedule.valid?).to eq true
+        end
+      end
+
+      context 'is invalid' do
+        it 'when scheduled before the track\'s start date' do
+          event_schedule = build(:event_schedule, event: @event, room: @room, start_time: Date.current - 1.hour)
+          expect(event_schedule.valid?).to eq false
+          expect(event_schedule.errors[:start_time]).to eq ["can't be before the track's start date (#{@track.start_date})"]
+        end
+
+        it 'when event ends after the track\'s end date' do
+          event_schedule = build(:event_schedule, event: @event, room: @room, start_time: Date.current + 1.day - 10.minutes)
+          expect(event_schedule.valid?).to eq false
+          expect(event_schedule.errors[:end_time]).to eq ["can't be after the track's end date (#{@track.end_date})"]
+        end
+      end
+    end
   end
 end
