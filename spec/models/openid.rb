c3872c9bd0cd2daec0f1a78b8b847a40d1c3bc2d
ViditChitkara <chitkara.vidit@gmail.com>
2017-10-21 02:38:41 +0530

    added email presence validation to openid
    closes #1658
    
    added test for openid
    
    fixed robocup offences
    
    written tests for email presence validation
    
    minor changes
    
    shifted omniauth callback tests to rspec controllers
    
    fixed linting bug
    
    minor changes
    
    minor changes

---
+++ spec/models/openid.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Openid do
+  subject { create(:openid) }
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:provider) }
+    it { is_expected.to validate_presence_of(:uid) }
+    it { is_expected.to validate_presence_of(:email) }
+  end
+
+  describe 'association' do
+    it { is_expected.to belong_to(:user) }
+  end
+end
