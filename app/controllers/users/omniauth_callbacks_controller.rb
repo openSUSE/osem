83053f4626a1a9ed26b4279ac286b7b5ff39e209
ViditChitkara <chitkara.vidit@gmail.com>
2017-10-21 02:38:41 +0530

    added email presence validation to openid
    closes #1658
    
    added test for openid
    
    fixed robocup offences
    
    written tests for email presence validation
    
    minor changes
    
    shifted omniauth callback tests to rspec controllers
    
    fixed linting bug
    
    minor changes
    
    minor changes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,6 +11,11 @@ module Users
 
     def handle(provider)
       auth_hash = request.env['omniauth.auth']
+      unless auth_hash.info.email.present?
+        flash[:error] = "Email field is missing in your #{provider} account"
+        redirect_to new_user_registration_path
+        return
+      end
       username = auth_hash.info.email.split('@')[0]
       openid = Openid.find_for_oauth(auth_hash) # Get or create openid
       # If openid exists and is associated with a user, sign in with associated user,
