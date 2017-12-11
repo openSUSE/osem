23ffc980c71aa04afb6b2a0565714559b9b6ca37
Naman Gupta <01namangupta@gmail.com>
2017-12-11 21:44:00 +0530

    optional abstract

--- app/models/event.rb
+++ app/models/event.rb
@@ -38,7 +38,7 @@ class Event < ActiveRecord::Base
   validate :abstract_limit
   validate :before_end_of_conference, on: :create
   validates :title, presence: true
-  validates :abstract, presence: true
+  validates :abstract, presence: true, if: 'event_type.minimum_abstract_length > 0'
   validates :event_type, presence: true
   validates :program, presence: true
   validates :speakers, presence: true
