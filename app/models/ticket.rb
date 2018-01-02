4896f1045e76739d1b70aa491c2f4d063f21e1f0
ViditChitkara <chitkara.vidit@gmail.com>
2018-01-02 19:29:28 +0530

    changes for test faliures

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -62,10 +62,10 @@ class Ticket < ApplicationRecord
 
   def tickets_turnover_total(id)
     tickets = TicketPurchase.where(ticket_id: id).paid
-    unless tickets. blank?
-      tickets.inject(Money.new(0, tickets.first.price_currency)){ |sum, ticket| sum + (ticket.amount_paid * ticket.quantity) }
-    else
+    if tickets.blank?
       Money.new(0, Ticket.find(id).price_currency)
+    else
+      tickets.inject(Money.new(0, tickets.first.price_currency)){ |sum, ticket| sum + (ticket.amount_paid * ticket.quantity) }
     end
   end
 
