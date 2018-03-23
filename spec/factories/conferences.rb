a1cc2f5495085fb5079159512d4ce62f40b0f507
Michael Klimenko <michaelkl@mail.ru>
2018-03-23 17:13:57 +0500

    Make factories and tests timezone-friendly

--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     title { Faker::Book.title }
     short_title { SecureRandom.urlsafe_base64(4) }
     timezone { Faker::Address.time_zone }
-    start_date { Date.current }
+    start_date { Time.zone.today }
     end_date { 6.days.from_now }
     start_hour 9
     end_hour 20
