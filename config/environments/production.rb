24c6b2bb1931d7669617e8d1b17bd38ab6c6827b
James Mason <jmason@suse.com>
2018-03-30 15:58:53 -0700

    Add ENV variable for production log level
    (cherry picked from commit f8856da0fd3c9e41330fdd09c407eb7f61b078dc)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Osem::Application.configure do
   config.force_ssl = !!ENV['FORCE_SSL']
 
   # See everything in the log (default is :info)
-  config.log_level = :info
+  config.log_level = ENV['OSEM_LOG_LEVEL'].try(:to_sym) || :info
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
