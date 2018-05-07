7c60910410db746625bb37bdea62f715c7a24884
James Mason <jmason@suse.com>
2018-05-07 16:47:29 -0700

    Add frozen_string_literal magic comment
    re: https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Style/FrozenStringLiteralComment

--- db/migrate/20180316220446_add_accepted_code_of_conduct_to_registrations.rb
+++ db/migrate/20180316220446_add_accepted_code_of_conduct_to_registrations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAcceptedCodeOfConductToRegistrations < ActiveRecord::Migration[5.0]
   def change
     add_column :registrations, :accepted_code_of_conduct, :boolean
