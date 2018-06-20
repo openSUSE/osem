c524f8cc6533f28e9a0883b944d962bfc978c853
James Mason <jmason@suse.com>
2018-06-20 15:17:27 -0700

    Add an option for shorter datatable pages

--- app/assets/javascripts/osem-datatables.js
+++ app/assets/javascripts/osem-datatables.js
@@ -3,7 +3,7 @@ $(function () {
     "stateSave": true,
     "autoWidth": false,
     "pagingType": "full_numbers",
-    "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]],
+    "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
   });
 
   $('.datatable:not([data-source])').DataTable();
