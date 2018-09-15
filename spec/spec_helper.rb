530754cae37ab86e0f15c108a4d1e6de8e1160d4
Ana María Martínez Gómez <ammartinez@suse.de>
2018-09-16 00:17:02 +0200

    Add Shoulda Matchers 3 configuration
    From version 3.0.0 it is needed to specfied which test framework you're
    using and which portion of the matchers you want to use in
    `rails_helper.rb` (or the old `spec_helper.rb` in our case).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -122,3 +122,10 @@ RSpec.configure do |config|
 end
 
 OmniAuth.config.test_mode = true
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
