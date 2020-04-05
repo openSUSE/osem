310fc8dc68279a071b1e987beb22bcde0e8b76ea
Andrew Kvalheim <Andrew@Kvalhe.im>
2020-04-05 11:16:15 -0700

    Fix bug in test database preparation
    Presumably the intent of this was to clear the database and repopulate
    it with seed data. `transaction` isn't the right strategy for this; it
    just rolls back any existing transactions.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,7 +2,7 @@
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:transaction)
+    DatabaseCleaner.clean_with(:truncation)
     Rails.application.load_seed
   end
 
