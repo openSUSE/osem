5b312459d0903b48a02834f9bc5e6ac9a94e40c7
Michael Ball <cycomachead@gmail.com>
2020-07-22 17:49:54 -0700

    sort happening at the same time events by room order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -112,7 +112,7 @@ module ApplicationHelper
         concurrent_events << other_event_schedule.event
       end
     end
-    concurrent_events
+    concurrent_events.sort_by { |event_schedule| event_schedule.room&.order }
   end
 
   def speaker_links(event)
