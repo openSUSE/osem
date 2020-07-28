c21c6e15251394fc266c428a3f160d035b40427d
Michael Ball <cycomachead@gmail.com>
2020-07-27 19:36:00 -0700

    I dont understand why this matters...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -190,7 +190,7 @@ module ApplicationHelper
 
   # TODO:Snap!Con: Replace this with a search for a conference logo.
   def nav_root_link_for(conference)
-    path = conference ? conference_path(conference) : root_path
+    path = conference.try(:name) ? conference_path(conference) : root_path
     link_to(
       image_tag('snapcon_logo.png'),
       path,
