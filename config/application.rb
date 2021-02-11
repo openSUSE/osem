46d1b5caf5e5b67151e29f121c050270e91d847b
CactusPuppy <puppycactus@gmail.com>
2021-02-10 17:10:18 -0800

    Add ability to use local environment variables via config/local_env.yml

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,12 @@ module Osem
     config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
 
     config.active_job.queue_adapter = :delayed_job
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
