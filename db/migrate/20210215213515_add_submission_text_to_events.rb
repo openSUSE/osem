974d12db30d70159d44aec7462f6575f04cde976
CactusPuppy <puppycactus@gmail.com>
2021-02-25 20:30:27 -0800

    Fix some rubocop complaints

--- db/migrate/20210215213515_add_submission_text_to_events.rb
+++ db/migrate/20210215213515_add_submission_text_to_events.rb
@@ -1,5 +1,5 @@
 class AddSubmissionTextToEvents < ActiveRecord::Migration[5.2]
   def change
-  	add_column :events, :submission_text, :text
+    add_column :events, :submission_text, :text
   end
 end
