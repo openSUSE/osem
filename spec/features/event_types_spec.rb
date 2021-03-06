bc4177154ad8f72df1ae974d2ac61382b3e2a616
Michael Ball <cycomachead@gmail.com>
2021-03-06 12:45:00 -0800

    Add editing submission instructions

--- spec/features/event_types_spec.rb
+++ spec/features/event_types_spec.rb
@@ -22,7 +22,8 @@ feature EventType do
 
       fill_in 'event_type_title', with: 'Party'
       fill_in 'event_type_length', with: '240'
-      fill_in 'event_type_description', with: '### This is a description'
+      fill_in 'event_type_description', with: '**Description**'
+      fill_in 'event_type_submission_instructions', with: '**Instructions**'
       fill_in 'event_type_minimum_abstract_length', with: '0'
       fill_in 'event_type_maximum_abstract_length', with: '13042'
       page.find('#event_type_color').set('#e4e4e4')
@@ -30,11 +31,14 @@ feature EventType do
       click_button 'Create Event type'
       page.find('#flash')
       # Validations
+      # binding.pry
       expect(flash).to eq('Event type successfully created.')
       within('table#event_types') do
         expect(page.has_content?('Party')).to be true
         expect(page.has_content?('13042')).to be true
         expect(page.has_content?('#E4E4E4')).to be true
+        expect(page.has_content?('Description')).to be true
+        expect(page.has_content?('Instructions')).to be true
         expect(page.assert_selector('tr', count: 3)).to be true
       end
 
