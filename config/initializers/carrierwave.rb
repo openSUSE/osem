1291d1846dd84c593f02c3d6f0d0c988947e5084
Henne Vogelsang <hvogel@opensuse.org>
2021-03-06 21:58:23 +0100

    Move cache to file
    No need to store caches in the cloud.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   config.storage = :file
+  config.cache_storage = :file
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.enable_processing = false if Rails.env.test?
 end
