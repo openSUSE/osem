d4e6122ba2e9394afe4c8472fd8083cf04a3f5bd
Henne Vogelsang <hvogel@opensuse.org>
2021-03-07 01:12:58 +0100

    Set a release for sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -9,4 +9,12 @@ Sentry.init do |config|
   # config.traces_sampler = lambda do |context|
   #  true
   # end
+
+  # During deployment we touch tmp/restart.txt, let's use its last access time as release.
+  # Unless someone has set a variable of course...
+  osem_version_from_file = nil
+  version_file = File.expand_path('../../tmp/restart.txt', __dir__)
+  osem_version_from_file = File.new(version_file).atime.to_i if File.file?(version_file)
+  osem_version = ENV.fetch('OSEM_SENTRY_RELEASE', osem_version_from_file)
+  config.release = osem_version if osem_version
 end
