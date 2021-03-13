7f60f8aace6daaca2915f38d3838cf2f0ec2ed11
Ziyi <lzy_1006@icloud.com>
2021-03-13 15:22:19 -0800

    Init create_lead, fix MailblusterHelper name

--- app/helpers/external/mailbluster_helper.rb
+++ app/helpers/external/mailbluster_helper.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 module External
-  module Mailbluster
+  module MailblusterHelper
     def create_lead(user)
       # TODO
+      begin
+        uri = URI("api.mailbluster.com/api/leads")
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.path,
+          {'Authorization' => ENV["MAILBLUSTER_API_KEY"]}) # TODO Authorization=APIKEY
+        request.body = {'firstName' => user.name,
+            'email' => user.email,
+            'subscribed' => true,
+            'tags' => ["snapcon"],
+            'overrideExisting' => true}.to_json
+        response = http.request(request)
+        puts response
+        return true
+      rescue => e
+        puts "ERROR #{e}"
+        return false
+      end
     end
   end
 end
