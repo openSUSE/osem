5739c05a940f3f0140cf5c3802e97e8382ef1235
CactusPuppy <puppycactus@gmail.com>
2021-03-13 15:04:57 -0800

    Begin writing tests for Mailbluster API calls

---
+++ spec/helpers/external/mailbluster_helper_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'webmock/rspec'
+
+describe MailblusterHelper, type: :helper do
+  let!(:user) { create(:user) }
+
+  describe 'create_lead' do
+    it 'makes a post request to Mailbluster\'s API' do
+      create_lead(@user)
+      expect(WebMock).to have_requested(:post, "api.mailbluster.com/api/leads").
+        with { |req| req.body == "abc" }
+    end
+  end
+end
