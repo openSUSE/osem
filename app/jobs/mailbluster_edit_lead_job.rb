b9ff3a25301d8bcd231a64e78a1fab2b08dd5f84
CactusPuppy <puppycactus@gmail.com>
2021-03-30 22:21:03 -0700

    Fix Rubocop linting issues

--- app/jobs/mailbluster_edit_lead_job.rb
+++ app/jobs/mailbluster_edit_lead_job.rb
@@ -5,6 +5,6 @@ class MailblusterEditLeadJob < ApplicationJob
 
   def perform(user, add_tags: [], remove_tags: [], old_email: nil)
     ApplicationController.helpers.edit_lead(user,
-      add_tags: add_tags, remove_tags: remove_tags, old_email: old_email)
+                                            add_tags: add_tags, remove_tags: remove_tags, old_email: old_email)
   end
 end
