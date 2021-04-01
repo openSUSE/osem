f0582c25bf5fe75c768c85d92a0b7814617691e3
CactusPuppy <puppycactus@gmail.com>
2021-03-31 21:05:59 -0700

    Move Mailbluster business logic to services/ instead of helpers/

--- app/jobs/mailbluster_delete_lead_job.rb
+++ app/jobs/mailbluster_delete_lead_job.rb
@@ -2,6 +2,6 @@ class MailblusterDeleteLeadJob < ApplicationJob
   queue_as :default
 
   def perform(user)
-    ApplicationController.helpers.delete_lead(user)
+    MailblusterManager.delete_lead(user)
   end
 end
