f41d0625b4371610c4c71321bfec2e562a083827
CactusPuppy <puppycactus@gmail.com>
2021-04-01 22:08:25 -0700

    Rubocop linting issues fixed

--- app/services/mailbluster_manager.rb
+++ app/services/mailbluster_manager.rb
@@ -1,7 +1,7 @@
 class MailblusterManager
   include HTTParty
   base_uri 'https://api.mailbluster.com/api/leads/'
-  @@auth_headers = {
+  @auth_headers = {
     headers: {
       'Content-Type'  => 'application/json',
       'Authorization' => ENV['MAILBLUSTER_API_KEY']
@@ -9,31 +9,28 @@ class MailblusterManager
   }
 
   def self.query_api(method, path, body)
-    #TODO
-    options = @@auth_headers.merge({
-      body:    body.to_json
-    })
+    options = @auth_headers.merge(body: body.to_json)
     send(method, path, options).parsed_response
   end
 
   def self.create_lead(user)
-    query_api(:post, '/', {
-      'email'            => user.email,
-      'firstName'        => user.name,
-      'overrideExisting' => true,
-      'subscribed'       => true,
-      'tags'             => [ENV['OSEM_NAME'] || 'snapcon']
-    })
+    query_api(:post, '/',
+              'email'            => user.email,
+              'firstName'        => user.name,
+              'overrideExisting' => true,
+              'subscribed'       => true,
+              'tags'             => [ENV['OSEM_NAME'] || 'snapcon']
+    )
   end
 
   def self.edit_lead(user, add_tags: [], remove_tags: [], old_email: nil)
     email_hash = Digest::MD5.hexdigest(old_email.presence || user.email)
-    query_api(:put, "/#{email_hash}", {
-        'email'      => user.email,
-        'firstName'  => user.name,
-        'addTags'    => add_tags,
-        'removeTags' => remove_tags
-    })
+    query_api(:put, "/#{email_hash}",
+              'email'      => user.email,
+              'firstName'  => user.name,
+              'addTags'    => add_tags,
+              'removeTags' => remove_tags
+    )
   end
 
   def self.delete_lead(user)
