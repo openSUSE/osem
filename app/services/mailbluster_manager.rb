d780c2a6b3e2765d655e925ae1cbafabdb3b7777
CactusPuppy <puppycactus@gmail.com>
2021-04-01 23:10:43 -0700

    Make body argument for query_api more clear

--- app/services/mailbluster_manager.rb
+++ app/services/mailbluster_manager.rb
@@ -8,33 +8,33 @@ class MailblusterManager
     }
   }
 
-  def self.query_api(method, path, body)
+  def self.query_api(method, path, body: {})
     options = @auth_headers.merge(body: body.to_json)
     send(method, path, options).parsed_response
   end
 
   def self.create_lead(user)
-    query_api(:post, '/',
-              'email'            => user.email,
-              'firstName'        => user.name,
-              'overrideExisting' => true,
-              'subscribed'       => true,
-              'tags'             => [ENV['OSEM_NAME'] || 'snapcon']
-    )
+    query_api(:post, '/', body: {
+                'email'            => user.email,
+                'firstName'        => user.name,
+                'overrideExisting' => true,
+                'subscribed'       => true,
+                'tags'             => [ENV['OSEM_NAME'] || 'snapcon']
+              })
   end
 
   def self.edit_lead(user, add_tags: [], remove_tags: [], old_email: nil)
     email_hash = Digest::MD5.hexdigest(old_email.presence || user.email)
-    query_api(:put, "/#{email_hash}",
-              'email'      => user.email,
-              'firstName'  => user.name,
-              'addTags'    => add_tags,
-              'removeTags' => remove_tags
-    )
+    query_api(:put, "/#{email_hash}", body: {
+                'email'      => user.email,
+                'firstName'  => user.name,
+                'addTags'    => add_tags,
+                'removeTags' => remove_tags
+              })
   end
 
   def self.delete_lead(user)
     email_hash = Digest::MD5.hexdigest user.email
-    query_api(:delete, "/#{email_hash}", {})
+    query_api(:delete, "/#{email_hash}")
   end
 end
