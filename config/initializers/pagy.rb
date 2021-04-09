185b2773d7e79ca16a5ad0d6b09e0145c6990042
Jimmy <jimmyxu1997@berkeley.edu>
2021-04-09 16:33:39 +0800

    [feat]Add bootstrap style to pagination navbar

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -36,7 +36,7 @@ require 'pagy/extras/array'
 
 # Bootstrap extra: Add nav, nav_js and combo_nav_js helpers and templates for Bootstrap pagination
 # See https://ddnexus.github.io/pagy/extras/bootstrap
-# require 'pagy/extras/bootstrap'
+require 'pagy/extras/bootstrap'
 
 # Bulma extra: Add nav, nav_js and combo_nav_js helpers and templates for Bulma pagination
 # See https://ddnexus.github.io/pagy/extras/bulma
