a2d775e36d8c36e09a554aae2c699bc144d75fa9
Jimmy <jimmyxu1997@gmail.com>
2021-04-22 01:24:14 +0800

    [fix]Remove duplicate code

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -183,16 +183,4 @@ class ProposalsController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :username)
   end
-
-  def load_happening_now
-    events_schedules_list = get_happening_now_events_schedules(@conference)
-    @is_happening_next = false
-    if events_schedules_list.empty?
-      events_schedules_list = get_happening_next_events_schedules(@conference)
-      @is_happening_next = true
-    end
-    @events_schedules_limit = EVENTS_PER_PAGE
-    @events_schedules_length = events_schedules_list.length
-    @pagy, @events_schedules = pagy_array(events_schedules_list, items: @events_schedules_limit, link_extra: 'data-remote="true"')
-  end
 end
