e1918fbf7ea98c87c2b0b70f4c72fce85ff14b3e
Jimmy <jimmyxu1997@gmail.com>
2021-04-26 09:48:16 +0800

    [feat]Implement FullCalendar#rooms_to_resources and add tests

--- app/services/full_calendar_formatter.rb
+++ app/services/full_calendar_formatter.rb
@@ -1,3 +1,16 @@
 class FullCalendarFormatter
-  def rooms_to_resources(rooms); end
+  def self.rooms_to_resources(rooms)
+    rooms.map { |room| room_to_resource(room) }.to_json
+  end
+
+  def self.event_schedules_to_resources(event_schedules); end
+
+  private_class_method
+
+  def self.room_to_resource(room)
+    {
+      id:    room.guid,
+      title: room.name
+    }
+  end
 end
