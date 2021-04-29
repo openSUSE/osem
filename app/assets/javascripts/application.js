dd6a2600e7da1fef21e1c228ce55c62b7e61c6a6
CactusPuppy <puppycactus@gmail.com>
2021-04-28 21:42:16 -0700

    Commit to using Yarn over NPM

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,7 +55,7 @@
 //= require bootstrap-select
 //= require osem-survey
 //= require pagy
-//= require fullcalendar-scheduler/main.js
+//= require @fullcalendar/common/main.js
 
 $(document).ready(function() {
     $('a[disabled=disabled]').click(function(event){
