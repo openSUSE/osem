813c28db461242612df42bd629d2339cb84c6827
Henne Vogelsang <hvogel@opensuse.org>
2021-08-17 14:58:06 +0200

    Run rails upgrade task
    next bundle exec rails app:update

--- config/initializers/ajax_datatables_rails.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-AjaxDatatablesRails.configure do |config|
-  config.db_adapter = Rails.configuration.database_configuration[Rails.env]['adapter'].to_sym
-  config.orm = :active_record
-end
