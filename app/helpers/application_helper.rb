0fb080441a7b59e1e5848c6e3bb75d3cad5643b0
Henne Vogelsang <hvogel@opensuse.org>
2022-02-15 13:30:35 +0100

    Drop redirect_back_or_to helper
    This is a thing in ActionController by now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -88,14 +88,6 @@ module ApplicationHelper
     end
   end
 
-  def redirect_back_or_to(options = {}, response_status = {})
-    if request.env['HTTP_REFERER']
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_to options, response_status
-    end
-  end
-
   def concurrent_events(event)
     return nil unless event.scheduled? && event.program.selected_event_schedules
 
