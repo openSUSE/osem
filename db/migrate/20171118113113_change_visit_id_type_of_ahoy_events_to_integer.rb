f56551dddf58482392aff378be2556bf9e07124d
David Sedeño <frimost@gmail.com>
2023-10-19 20:17:11 +0200

    Remove old and unused migrations. #1

--- db/migrate/20171118113113_change_visit_id_type_of_ahoy_events_to_integer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeVisitIdTypeOfAhoyEventsToInteger < ActiveRecord::Migration[5.0]
-  def change
-    change_column :ahoy_events, :visit_id, :integer, limit: nil
-  end
-end
