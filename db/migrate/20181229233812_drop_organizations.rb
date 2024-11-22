81cdec6633740c51b49b7acab7a7d5624dbf7208
Henne Vogelsang <hvogel@opensuse.org>
2024-11-22 14:15:05 +0100

    First remove reference, then drop table...

--- db/migrate/20181229233812_drop_organizations.rb
+++ db/migrate/20181229233812_drop_organizations.rb
@@ -1,11 +1,11 @@
 class DropOrganizations < ActiveRecord::Migration[7.0]
   def change
+    remove_reference :conferences, :organization, index: true
+
     drop_table :organizations do |t|
       t.string :name, null: false
       t.text :description
       t.string :picture
     end
-
-    remove_reference :conferences, :organization, index: true
   end
 end
