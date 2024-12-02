a447ad90c4417bed69c91ff82816236932dacdd9
Henne Vogelsang <hvogel@opensuse.org>
2024-12-02 13:17:05 +0100

    Autocorrect Rails/RootPathnameMethods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ include ERB::Util
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
 RSpec.configure do |config|
   # ## Mock Framework
