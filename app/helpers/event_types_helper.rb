9514171e33d55a4989768d8ea9e71e1f3bd41b14
Henne Vogelsang <hvogel@opensuse.org>
2025-01-30 17:23:36 +0100

    Simplify min/max abstract length feature
    Also clarify that those are words...

--- app/helpers/event_types_helper.rb
+++ app/helpers/event_types_helper.rb
@@ -8,6 +8,6 @@ module EventTypesHelper
   # ====Returns
   # * +String+ -> number of registrations / max allowed registrations
   def event_type_select_options(event_types = {})
-    event_types.map { |type| ["#{type.title} - #{show_time(type.length)}", type.id] }
+    event_types.map { |type| ["#{type.title} - #{show_time(type.length)} - #{type.minimum_abstract_length} to  #{type.maximum_abstract_length} words", type.id] }
   end
 end
